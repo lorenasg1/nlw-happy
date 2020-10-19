4170ef69d07378162b57a5090ae0248aa3f17ee9
lorenasg1 <lorenasg1@gmail.com>
2020-10-19 14:21:09 -0300

    fix: rename routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import CreateOrphanage from '../pages/CreateOrphanage';
 import Landing from '../pages/Landing';
 import Orphanage from '../pages/Orphanage';
 import OrphanagesMap from '../pages/OrphanagesMap';
+import Success from '../pages/Success';
 
 const Routes = () => {
   return (
@@ -12,7 +13,8 @@ const Routes = () => {
         <Route path="/" component={Landing} exact />
         <Route path="/app" component={OrphanagesMap} />
 
-        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/create" component={CreateOrphanage} exact />
+        <Route path="/orphanages/create/success" component={Success} />
         <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
